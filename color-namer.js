1579f7daee94c68ed101406016cf0fa25e7c2bb2
hayfield <hayfield@gmx.com>
2011-04-19 09:00:50 +0100

    Draw an image to the canvas

--- color-namer.js
+++ color-namer.js
@@ -7,12 +7,30 @@ var Namer = {
     canvas: null,
     
     /**
+        The canvas context
+    */
+    context: null,
+    
+    /**
+        Draws an image with the specified fiel path in the top-left corner
+    */
+    drawImage: function( filePath ){
+        var img = new Image();
+        img.src = filePath;
+        img.onload = function() {
+            context.drawImage(img, 0, 0);
+        };
+    },
+    
+    /**
         Initialises the color namer
     */
     init: function(){
         canvas = document.getElementById("mainCanvas");
+        context = canvas.getContext("2d");
         canvas.width = document.width;
         canvas.height = document.height;
+        Namer.drawImage("img.png");
     }
 
 };
@@ -20,3 +38,17 @@ var Namer = {
 $(function() {
 	Namer.init();
 });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
